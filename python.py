533532a2747f41ca4ba7bac3b6df1e01813049f7
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-28 11:41:30 +0400

    Age requirement

--- python.py
+++ python.py
@@ -3,6 +3,10 @@ height = int(input("What is your Height in cm"))
 
 if height>120:
     print("You can buy a ticket")
+    age = int(input("what is your age?"))
+    if age<=18:
+        print("Please pay 10$")
+    else:
+        print("Please pay 15$")
 else:
     print("Sorry, You can't buy it because the neck is small")
-    

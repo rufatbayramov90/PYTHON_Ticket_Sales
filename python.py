bc6f4d67fdd16cc403e21e630407d7573761b0fb
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-28 11:20:01 +0400

    when height is 100 cm

---
+++ python.py
@@ -0,0 +1,8 @@
+print("welcome to the checkout")
+height = int(input("What is your Height in cm"))
+
+if height>120:
+    print("You can buy a ticket")
+else:
+    print("Sorry, You can't buy it because the neck is small")
+    

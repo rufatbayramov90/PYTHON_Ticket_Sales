81c23ea7e1e01651832ac0bd083a61d39f369bf3
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-09-28 11:46:26 +0400

    Conditions with elif

--- python.py
+++ python.py
@@ -4,8 +4,12 @@ height = int(input("What is your Height in cm"))
 if height>120:
     print("You can buy a ticket")
     age = int(input("what is your age?"))
-    if age<=18:
+    if age < 12:
+        print("Please pay 7$")
+    elif age <= 18:
         print("Please pay 10$")
+    elif age > 18:
+        print("Please pay 24$")
     else:
         print("Please pay 15$")
 else:

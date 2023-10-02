ecd04fe18e18f26805ce3f8117583320d17f03b2
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-02 11:03:13 +0400

    Pizza deliveries

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-print("welcome to the checkout")
+'''print("welcome to the checkout")
 height = int(input("What is your Height in cm"))
 bill = 0
 if height>120:
@@ -21,3 +21,30 @@ if height>120:
     print(f"Your final bill is ${bill}")
 else:
     print("Sorry, You can't buy it because the neck is small")
+'''
+
+
+print("Would you like to buy pizza")
+bill = 0
+size = input("What size pizza do you want? S, M or L")
+if size =="S":
+    bill = 15
+    print(f"Please pay ${bill}")
+elif size =="M":
+    bill = 20
+    print(f"Please pay ${bill}")
+else:
+    bill = 25
+    print(f"Please pay ${bill}")
+
+add_pepperoni = input("Do you want pepperoni? Y or N")
+if add_pepperoni =="Y":
+    bill += 5
+    print(f"Please pay ${bill}")
+
+add_cheese = input("Do you want extra cheese? Y or N")
+if add_cheese =="Y":
+    bill += 10
+    print(f"Please pay ${bill}")
+else:
+    print(f"Your final bill ${bill}")
